0760a5dc1b018c27b637cc415bb03776595d9ccd
Aleksandr <keytolife94@gmail.com>
2024-05-18 21:56:36 -0500

    Update button and js for change theme

---
+++ assets/js/logic.js
@@ -0,0 +1,32 @@
+const themeBtn = document.querySelector(`#background-theme`);
+
+function init() {
+    if (localStorage.getItem(`theme`) === null) {
+        localStorage.setItem(`theme`, true)
+    }
+    changeTheme()
+}
+
+function changeTheme() {
+    if (localStorage.getItem(`theme`) === `true`) {
+        document.documentElement.style.setProperty('--light', '#17171b');
+        document.documentElement.style.setProperty('--dark', '#f8f9fa');
+        document.documentElement.style.setProperty('--gray', '#fff');
+        document.documentElement.style.setProperty('--gray-dark', 'antiquewhite');
+    } else {
+        document.documentElement.style.setProperty('--light', '#f8f9fa');
+        document.documentElement.style.setProperty('--dark', '#17171b');
+        document.documentElement.style.setProperty('--gray', '#6c757d');
+        document.documentElement.style.setProperty('--gray-dark', '#343a40');
+    }
+}
+
+themeBtn.addEventListener(`click`, function () {
+    if (localStorage.getItem(`theme`) === null || localStorage.getItem(`theme`) === `false`) {
+        localStorage.setItem(`theme`, true);
+    } else {
+        localStorage.setItem(`theme`, false);
+    }
+    changeTheme();
+})
+init();
